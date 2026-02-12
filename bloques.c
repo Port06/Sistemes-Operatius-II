e83607615a73ae5d62532bb50ae974f176fdbb59
Port06 <115357047+Port06@users.noreply.github.com>
2026-02-12 19:52:27 +0100

    Bwrite

--- bloques.c
+++ bloques.c
@@ -14,9 +14,20 @@ int int bmount(const char *camino); {
 
 int bumount() {
 	
-	close();
+	int close(int descriptor);
 };
 
-int bwrite(unsigned int nbloque, const void *buf) {};
+//Funcion encargada de escribir un bloque en el dispositivo virtual
+int bwrite(unsigned int nbloque, const void *buf) {
+	
+	//Se calcula el desplazamiento para acceder al byte correcto para la escritura
+	int desplazamiento = nbloque * BLOCKSIZE;
+	
+	//Se mueve la posicion del puntero para que este bien posicionado
+	off_t lseek(int descriptor, off_t desplazamiento, int punto_de_referencia);
+	
+	//Finalmente se essribe el bloque completo reeemplazando el anterior
+	size_t write(int descriptor, const void *buf, size_t nbytes);
+};
 
 int bread(unsigned int nbloque, void *buf) {};
