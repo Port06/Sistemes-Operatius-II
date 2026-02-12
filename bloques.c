7389e0f8917b9d1364103114e77a8cf3b95c3ddb
Port06 <115357047+Port06@users.noreply.github.com>
2026-02-12 19:22:09 +0100

    .

--- bloques.c
+++ bloques.c
@@ -5,3 +5,12 @@ int blockSize = 1024;
 
 int bmount(const char *camino) {};
 
+//Se crea el fichero
+int bumount() {
+	
+	int open(const char *camino, int O_RDWR, mode_t mode);
+};
+
+int bwrite(unsigned int nbloque, const void *buf) {};
+
+int bread(unsigned int nbloque, void *buf) {};
