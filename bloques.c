28f2d30e0420726b5304bb7081b4b76ffcb43f19
Port06 <115357047+Port06@users.noreply.github.com>
2026-02-16 12:00:21 +0100

    Bloques.c corregido

--- bloques.c
+++ bloques.c
@@ -2,49 +2,82 @@
 
 #include "bloques.h"
 
-const int BLOCKSIZE = 1024;
+//Inicializacion de variables
 static int descriptor = 0;
 
 
 //Se crea el fichero
-int int bmount(const char *camino); {
+int bmount(const char *camino) {
 	
-	int open(const char *camino, int O_RDWR, mode_t 0666);
-	perror();
+	descriptor = open(camino, O_RDWR | O_CREAT, 0666);
+	
+	//Manejo de errores al abrir el descriptor
+	if (descriptor == -1) {
+		perror();
+		return FALLO;
+	}
+	return descriptor;
 };
 
 //Se cierra el fichero
 int bumount() {
 	
-	int close(int descriptor);
-	perror();
+	//Manejo e errores al cerrar el descriptor
+	if (close(descriptor) == -1) {
+		perror();
+		return FALLO;
+	}
+	return EXITO;
 };
 
 //Funcion encargada de escribir un bloque en el dispositivo virtual
 int bwrite(unsigned int nbloque, const void *buf) {
 	
 	//Se calcula el desplazamiento para acceder al byte correcto para la escritura
-	int desplazamiento = nbloque * BLOCKSIZE;
+	off_t desplazamiento = nbloque * BLOCKSIZE;
 	
 	//Se mueve la posicion del puntero para que este bien posicionado
-	off_t lseek(int descriptor, off_t desplazamiento, int punto_de_referencia);
+	if (lseek(descriptor, desplazamiento, SEEK_SET) == -1) {
+        perror();
+        return FALLO;
+    }
 	
 	//Finalmente se essribe el bloque completo reeemplazando el anterior
-	size_t write(int descriptor, const void *buf, size_t nbytes);
-	perror();
+	int bytes_escritos = write(descriptor, buf, BLOCKSIZE);
+	
+	//Verificamos que se hayan escrito los bytes correctos
+	if (bytes_escritos != BLOCKSIZE) {
+		fprintf(stderr, "Escritura incompleta\n");
+		return FALLO;
+	} else if (bytes_escritos == -1) {
+        perror();
+        return FALLO;
+    }
+	
+	return bytes_escritos;
 };
 
 int bread(unsigned int nbloque, void *buf) {
-	
-	void *buf = malloc(BLOCKSIZE * sizeof(char));
-	
 	//Se calcula el desplazamiento para acceder al byte correcto para la lectura
-	int desplazamiento = nbloque * BLOCKSIZE;
+	off_t desplazamiento = nbloque * BLOCKSIZE;
 	
 	//Se mueve la posicion del puntero para que este bien posicionado
-	off_t lseek(int descriptor, off_t desplazamiento, int punto_de_referencia);
+	if (lseek(descriptor, desplazamiento, SEEK_SET) == -1) {
+        perror();
+        return FALLO;
+    }
 	
 	//Se efectua la lectura del bloque completo y se almacena en el buffer
-	buf = size_t read(int descriptor, void *buf, size_t nbytes);
-	perror();
+	int bytes_leidos = read(descriptor, buf, BLOCKSIZE);
+	
+	//Verificamos que se hayan leido los bytes correctos
+	if (bytes_leidos != BLOCKSIZE) {
+		fprintf(stderr, "Lectura incompleta\n");
+		return FALLO;
+	} else if (bytes_leidos == -1) {
+        perror();
+        return FALLO;
+    }
+	
+	return bytes_leidos;
 };
