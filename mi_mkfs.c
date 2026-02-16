4e9ebe88617ab093f2472d97f0386a98eadb320d
joantene <joantene05@gmail.com>
2026-02-16 22:42:24 +0100

    mañana sigo

--- mi_mkfs.c
+++ mi_mkfs.c
@@ -1,3 +1,14 @@
 //Archivo mi_mkfs.c
 
 #include "bloques.h"
+
+int main(int argc, char **argv) {
+
+    if(bmount(argv[2]) == FALLO) {
+        fprintf(stderr, RED "Error al montar el dispositivo" RESET);
+    }
+
+    for (int i=0; i<argv[1]; i++) {
+        
+    }
+}
