96150e90819eab6bf08bc0ffe0800518a09d7716
joantene <joantene05@gmail.com>
2026-02-17 11:31:49 +0100

    mi_mkfs

--- mi_mkfs.c
+++ mi_mkfs.c
@@ -3,12 +3,26 @@
 #include "bloques.h"
 
 int main(int argc, char **argv) {
+    //inicializacion de Buffer inicializado todo a 0's
+    unsigned char buffer0 = memset(buffer, 0, BLOCKSIZE)
 
-    if(bmount(argv[2]) == FALLO) {
+    //Montamos el dispositivo virtual
+    if(bmount(argv[1]) == FALLO) {
         fprintf(stderr, RED "Error al montar el dispositivo" RESET);
+        return FALLO
     }
 
-    for (int i=0; i<argv[1]; i++) {
-        
+    //Inicializamos a 0's el fichero utilizado como dispositivo virtual
+    for (int i=0; i<atoi(argv[2]); i++) {        
+        if (bwrite(i, buffer0) == FALLO) { //Si hay error al escribir desmontamos el dispositivo
+           fprintf(stderr, RED "Error inicializar el dispositivo a 0's" RESET);
+           bumount();
+           return FALLO
+        }
+    }
+
+    //Desmontamos el dispositivo virtual
+    if (bumount() == FALLO) {
+        return FALLO
     }
 }
