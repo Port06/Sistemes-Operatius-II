c2e58346475331b4bb077836151ee80b99a64607
Port06 <115357047+Port06@users.noreply.github.com>
2026-02-19 19:03:38 +0100

    tamMB funcional

--- fichero_basico.c
+++ fichero_basico.c
@@ -7,5 +7,17 @@
 int initMB();
 int initAI();
 int initSB(unsigned int nbloques, unsigned int ninodos);
-int tamMB(unsigned int nbloques);
+
+//Metodo para clacular el numero necessario de bloques
+int tamMB(unsigned int nbloques) {
+	int bloques = 0;
+	
+	//Se calcula el numero de bloques con fraccion
+	bloques = (nbloques / 8) / BLOCKSIZE;
+	
+	//Luego se incremente el numero de bloques en 1 si no es division eacta
+	if (((nbloques / 8) % BLOCKSIZE) != 0) bloques++;
+	
+	return bloques;
+};
 int tamAI(unsigned int ninodos);
