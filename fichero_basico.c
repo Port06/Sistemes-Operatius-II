269442ccd4c81873dfa04e3847a133d113839dd6
Port06 <115357047+Port06@users.noreply.github.com>
2026-02-19 20:05:29 +0100

    initMB

--- fichero_basico.c
+++ fichero_basico.c
@@ -4,9 +4,6 @@
 #include "fichero_basico.h"
 #include <time.h>
 
-int initMB();
-int initAI();
-int initSB(unsigned int nbloques, unsigned int ninodos);
 
 //Metodo para clacular el numero necessario de bloques
 int tamMB(unsigned int nbloques) {
@@ -32,6 +29,22 @@ int tamAI(unsigned int ninodos) {
 	if (((ninodos / 8) % BLOCKSIZE) != 0) tamAI++;
 	
 };
+
+//Metodo que inizializa a los bits del los metadatos
+int initMB() {
+	
+	unsigned int bloquesMetadatos;
+	bloquesMetadatos = tamSB + tamMB + tamAI;
+
+	unsigned int bitsMetadatos;
+	unsigned int bytesCompletos;
+	unsigned int bitsResto;
+
+	bitsMetadatos = bloquesMetadatos;     //Cada bloque es 1 bit
+
+	bytesCompletos = bitsMetadatos / 8;   //Los bytes completos a 11111111
+	bitsResto      = bitsMetadatos % 8;   //Los bits sueltos del siguiente byte
+}; 
 
 //Metodo que iniziliza el superbloque
 int initSB(unsigned int nbloques, unsigned int ninodos) {
