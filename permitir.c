0416f582a31e7a88286d65eb3ef0bbc59f06e609
Port06 <115357047+Port06@users.noreply.github.com>
2026-03-12 19:47:37 +0100

    permitir.c hecho

--- permitir.c
+++ permitir.c
@@ -1,1 +1,32 @@
-#include “ficheros.h”
+#include <stdio.h>
+#include <stdlib.h>
+#include "ficheros.h"
+
+int main(int argc, char **argv) {
+
+    // Validación de sintaxis
+    if (argc != 4) {
+        fprintf(stderr, "Sintaxis: ./permitir <nombre_dispositivo> <ninodo> <permisos>\n");
+        return -1;
+    }
+
+    // Montar dispositivo
+    if (bmount(argv[1]) == -1) {
+        fprintf(stderr, "Error al montar el dispositivo\n");
+        return -1;
+    }
+
+    // Convertir argumentos
+    unsigned int ninodo = atoi(argv[2]);
+    unsigned char permisos = atoi(argv[3]);
+
+    // Llamar a mi_chmod_f
+    if (mi_chmod_f(ninodo, permisos) == -1) {
+        fprintf(stderr, "Error al cambiar permisos\n");
+    }
+
+    // Desmontar dispositivo
+    bumount();
+
+    return 0;
+}
